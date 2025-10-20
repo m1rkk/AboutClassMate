2adb44f356139c06dbf241d9b48e494c58c47a5d
m1rkk <mrakkus15@gmail.com>
2025-10-20 09:16:42 +0300

    initial commit

---
+++ src/script.js
@@ -0,0 +1,6 @@
+document.getElementById("darkModeBtn").addEventListener("click", ev =>  {
+    document.getElementById("body").classList.toggle("dark");
+});
+document.getElementById("lightModeBtn").addEventListener("click", ev =>  {
+    document.getElementById("body").classList.toggle("dark");
+});
